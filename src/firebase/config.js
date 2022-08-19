aea3b1ca056ce4fa6d4807edecd8125fc66a6bf6
Mario Stosic <65606596+maki357@users.noreply.github.com>
2022-08-19 19:17:20 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmZ4hpa82YBFCvuG2tQeYjWcS1UeGNdSk",
+  apiKey: API_KEY,
   authDomain: "cooking-98de6.firebaseapp.com",
   projectId: "cooking-98de6",
   storageBucket: "cooking-98de6.appspot.com",
   messagingSenderId: "603763789620",
-  appId: "1:603763789620:web:c475e909e08a96c9669f61",
+  appId: API_ID,
 };
 
 // init firebase
